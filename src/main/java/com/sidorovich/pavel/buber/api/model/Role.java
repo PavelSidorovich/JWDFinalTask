02e14c68ee0eb99bf6b125828db9b4ef1b049e4f
Pavel Sidorovich <pavsid2001@mail.ru>
2021-11-20 22:26:41 +0300

    add unauthorized role

--- src/main/java/com/sidorovich/pavel/buber/api/model/Role.java
+++ src/main/java/com/sidorovich/pavel/buber/api/model/Role.java
@@ -1,12 +1,16 @@
 package com.sidorovich.pavel.buber.api.model;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 public enum Role {
     ADMIN("admin", 1),
     DRIVER("driver", 2),
-    CLIENT("client", 3);
+    CLIENT("client", 3),
+    UNAUTHORIZED("unauthorized", 4);
+
+    private static final List<Role> ALL_AVAILABLE_ROLES = Arrays.asList(values());
 
     private final String name;
     private final int id;
@@ -14,6 +18,10 @@ public enum Role {
     Role(String name, int id) {
         this.name = name;
         this.id = id;
+    }
+
+    public static List<Role> valuesAsList() {
+        return ALL_AVAILABLE_ROLES;
     }
 
     public static Optional<Role> getRoleById(int id) {
